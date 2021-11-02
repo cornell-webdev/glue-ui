4240e0a4fc1a14ec83d7e12d1257439f4bde6400
MelindaFang-code <67951224+MelindaFang-code@users.noreply.github.com>
2021-11-02 16:32:02 -0400

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Button from './components/Button/Button'
 import GlueProvider from './components/GlueProvider/GlueProvider'
 import IconButton from './components/IconButton/IconButton'
+import Tag from './components/Tag/Tag'
 
-export { Button, GlueProvider, IconButton }
+export { Button, GlueProvider, IconButton, Tag }
