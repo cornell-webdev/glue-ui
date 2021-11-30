4eb01a0853a69d6f698358e8e4a6913e55550529
MelindaFang-code <67951224+MelindaFang-code@users.noreply.github.com>
2021-11-30 16:41:07 -0500

    add tag component

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,40 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
+import ContainedTag from './ContainedTag'
+import OutlinedTag from './OutlinedTag'
 
 interface TagProps extends React.HTMLAttributes<HTMLDivElement> {
   background?: string
   color?: string
+  variant?: 'contained' | 'outlined'
+  disabled?: boolean
 }
 
-export const Tag = ({ children, ...rest }: TagProps) => {
-  return (
-    <Container {...rest}>
-      {children}
-    </Container>
-  )
+export const Tag = ({ children, variant = 'contained', ...rest }: TagProps) => {
+  switch (variant) {
+    case 'contained':
+      return <ContainedTag {...rest}>{children}</ContainedTag>
+
+    case 'outlined':
+      return <OutlinedTag {...rest}>{children}</OutlinedTag>
+  }
 }
-
-interface IContainerProps {
-  background: TagProps['background']
-  color: TagProps['color']
-}
-
-const Container = styled.div<IContainerProps>`
-  border-radius: 6px;
-  font-size: 0.88rem;
-  font-weight: medium;
-  padding: 0.5rem 0.8rem;
-  width: fit-content;
-  height: fit-content;
-  text-align: center;
-  color: ${(props) => props.theme.brand[500]};
-  border: none;
-  background: ${(props) => props.theme.brand[100]};;
-
-  /* background */
-  background: ${(props) => props.background && props.background};
-  /* color */
-  color: ${(props) => props.color && props.color}; 
-`
 
 export default Tag
