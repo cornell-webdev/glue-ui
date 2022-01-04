88941de28ddd339ad1a02d7354d580c84db56fd9
Jay Joo <jj534@cornell.edu>
2022-01-04 09:42:57 -0500

    Prevent size changes due to flex container

--- src/components/Spacer/Spacer.tsx
+++ src/components/Spacer/Spacer.tsx
@@ -6,6 +6,8 @@ interface ISpacerProps {
 }
 
 export const Spacer = styled.div<ISpacerProps>`
+  flex-grow: 0;
+  flex-shrink: 0;
   width: ${(props) => props.x && (typeof props.x === 'string' ? props.x : `${props.x / 2}rem`)};
   height: ${(props) => props.y && (typeof props.y === 'string' ? props.y : `${props.y / 2}rem`)};
 `
