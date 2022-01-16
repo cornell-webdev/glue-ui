d8c3aaecdbcb76d6519f4eb32566949ec1f25d7e
Jay Joo <jj534@cornell.edu>
2022-01-16 12:42:49 -0500

    Match contained tag dimensions with outlined tag

--- src/components/Tag/ContainedTag.tsx
+++ src/components/Tag/ContainedTag.tsx
@@ -19,22 +19,22 @@ interface IContainerProps {
   color?: ContainedTagProps['color']
 }
 
-const Container = styled.div<IContainerProps>`
+const Container = styled.button<IContainerProps>`
   border-radius: 20px;
   font-size: 12px;
   font-weight: medium;
   padding: 4px 11px;
-  width: fit-content;
-  height: fit-content;
   text-align: center;
   color: ${(props) => props.theme.brand[500]};
-  border: none;
+  border: 2px solid ${(props) => props.theme.brand[100]};
   background: ${(props) => props.theme.brand[100]};
 
   /* background */
   background: ${(props) => props.background && props.background};
+  border-color: ${(props) => props.background && props.background};
+
   /* color */
-  color: ${(props) => props.color && props.color}; 
+  color: ${(props) => props.color && props.color};
 `
 
 export default ContainedTag
